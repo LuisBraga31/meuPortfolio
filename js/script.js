9c50a8f37c1fb845169d79e4c1267b2015185091
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-08-07 15:09:12 -0300

    creating animation in intro section

--- js/script.js
+++ js/script.js
@@ -1,37 +1,12 @@
-var balls = document.querySelector('.balls');
-var quant = document.querySelectorAll('.slides .image');
-var atual = 0;
-var imagem = document.getElementById('atual');
+const title = document.querySelector('.luisbragadev');
 
-
-for(let i=0; i<quant.length; i++) {
-    var div = document.createElement('div');
-    div.id = i;
-    balls.appendChild(div);
-}
-document.getElementById('0').classList.add('imgAtual');
-
-var pos = document.querySelectorAll('.balls div');
-
-for(let i = 0; i < pos.length; i++) {
-    pos[i].addEventListener('click', ()=>{
-        atual = pos[i].id;
-        slide()
-    })
+function typeWriter(element) {
+    const arrayText = element.innerHTML.split('');
+    element.innerHTML = '';
+    arrayText.forEach((letra, i) => {
+        setTimeout(() => element.innerHTML += letra, 75 * i);
+    });
+    
 }
 
-function slide() {
-    if(atual >= quant.length){
-        atual = 0;
-    }
-    else if(atual < 0){
-        atual = quant.lenght -1;
-    }
-    document.querySelector('.imgAtual').classList.remove('imgAtual');
-    imagem.style.marginLeft = -520*atual+'px';
-    document.getElementById(atual).classList.add('imgAtual');
-
-}
-
-
-slide()
+typeWriter(title);
